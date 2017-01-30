6f5569bed23cf60b98078cd68918eec9e4bc245d
WillOB <obrienwp@s.dcsdk12.org>
2017-01-29 21:13:01 -0700

    Finish activity three.

---
+++ answers.md
@@ -0,0 +1,6 @@
+#Answers
+##Exercises
+Keyword/Response car/Do you like cars?; how are you/Fine, how are you"; computer/Hey, I'm a computer!
+To prioritize certain keywords, put them higher in the list of else if statements.
+##Question
+The computer counts keywords even if they are included in other words, so if you enter "know," the computer will see "no" and use that keyword.
--- src/Magpie2.java
+++ src/Magpie2.java
@@ -31,13 +31,28 @@ public class Magpie2 {
 	 */
 	public String getResponse(String statement) {
 		String response = "";
-		if (statement.indexOf("no") >= 0) {
+		if (statement.length() == 0){
+			response = "Say something, please.";
+		} else if (statement.indexOf("no") >= 0) {
 			response = "Why so negative?";
 		} else if (statement.indexOf("mother") >= 0
 				|| statement.indexOf("father") >= 0
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
+		} else if (statement.indexOf("cat") >= 0
+				|| statement.indexOf("dog") >= 0) {
+			response = "Tell me more about your pets.";
+		} else if (statement.indexOf("Mr.") >= 0) {
+			response = "He sounds like a good teacher!";
+		} else if (statement.indexOf("Mrs.") >= 0){
+			response = "She sounds like a good teacher!";
+		} else if (statement.indexOf("car") >= 0){
+			response = "Do you like cars?";
+		} else if (statement.indexOf("how are you") >= 0){
+			response = "Fine, how are you?";
+		} else if (statement.indexOf("computer") >= 0){
+			response = "Hey, I'm a computer!";
 		} else {
 			response = getRandomResponse();
 		}
@@ -50,7 +65,7 @@ public class Magpie2 {
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -63,6 +78,10 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4){
+			response = "Incredible.";
+		} else if (whichResponse == 5) {
+			response = "Wowwie!";
 		}
 
 		return response;
